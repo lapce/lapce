094faf8f18bb6f0e1f031563551aec391fc8302c
Dániel Buga <bugadani@gmail.com>
2022-04-02 09:50:41 +0200

    Add undo related code

--- lapce-data/src/buffer/data.rs
+++ lapce-data/src/buffer/data.rs
@@ -1,11 +1,14 @@
 use lapce_rpc::buffer::BufferId;
+use std::borrow::Cow;
 use std::cmp::Ordering;
 use std::sync::atomic::{self, AtomicU64};
 use std::{collections::BTreeSet, sync::Arc};
+use xi_rope::Delta;
 use xi_rope::{multiset::Subset, rope::Rope, DeltaBuilder, RopeDelta};
 
 use crate::buffer::{
-    shuffle, BufferContent, Contents, EditType, InvalLines, Revision,
+    shuffle, shuffle_tombstones, BufferContent, Contents, EditType, InvalLines,
+    Revision,
 };
 use crate::movement::Selection;
 
@@ -31,6 +34,7 @@ pub struct BufferData<L> {
 
     revs: Vec<Revision>,
     cur_undo: usize,
+    undos: BTreeSet<usize>,
     undo_group_id: usize,
     live_undos: Vec<usize>,
     deletes_from_union: Subset,
@@ -193,5 +197,170 @@ impl<L: BufferDataListener> BufferData<L> {
             inval_count: old_hard_count,
             new_count: new_hard_count,
         });
+    }
+
+    pub fn do_undo(&mut self) -> Option<RopeDelta> {
+        if self.cur_undo > 1 {
+            self.cur_undo -= 1;
+            self.undos.insert(self.live_undos[self.cur_undo]);
+            self.last_edit_type = EditType::Undo;
+            Some(self.undo(self.undos.clone()))
+        } else {
+            None
+        }
+    }
+
+    pub fn do_redo(&mut self) -> Option<RopeDelta> {
+        if self.cur_undo < self.live_undos.len() {
+            self.undos.remove(&self.live_undos[self.cur_undo]);
+            self.cur_undo += 1;
+            self.last_edit_type = EditType::Redo;
+            Some(self.undo(self.undos.clone()))
+        } else {
+            None
+        }
+    }
+
+    fn undo(&mut self, groups: BTreeSet<usize>) -> RopeDelta {
+        let (new_rev, new_deletes_from_union) = self.compute_undo(&groups);
+        let delta = Delta::synthesize(
+            &self.tombstones,
+            &self.deletes_from_union,
+            &new_deletes_from_union,
+        );
+        let new_text = delta.apply(&self.rope);
+        let new_tombstones = shuffle_tombstones(
+            &self.rope,
+            &self.tombstones,
+            &self.deletes_from_union,
+            &new_deletes_from_union,
+        );
+        self.undone_groups = groups;
+
+        if self.listener.should_apply_edit() {
+            self.apply_edit(
+                &delta,
+                new_rev,
+                new_text,
+                new_tombstones,
+                new_deletes_from_union,
+            );
+        }
+
+        delta
+    }
+
+    fn deletes_from_union_before_index(
+        &self,
+        rev_index: usize,
+        invert_undos: bool,
+    ) -> Cow<Subset> {
+        let mut deletes_from_union = Cow::Borrowed(&self.deletes_from_union);
+        let mut undone_groups = Cow::Borrowed(&self.undone_groups);
+
+        // invert the changes to deletes_from_union starting in the present and working backwards
+        for rev in self.revs[rev_index..].iter().rev() {
+            deletes_from_union = match rev.edit {
+                Contents::Edit {
+                    ref inserts,
+                    ref deletes,
+                    ref undo_group,
+                    ..
+                } => {
+                    if undone_groups.contains(undo_group) {
+                        // no need to un-delete undone inserts since we'll just shrink them out
+                        Cow::Owned(deletes_from_union.transform_shrink(inserts))
+                    } else {
+                        let un_deleted = deletes_from_union.subtract(deletes);
+                        Cow::Owned(un_deleted.transform_shrink(inserts))
+                    }
+                }
+                Contents::Undo {
+                    ref toggled_groups,
+                    ref deletes_bitxor,
+                } => {
+                    if invert_undos {
+                        let new_undone = undone_groups
+                            .symmetric_difference(toggled_groups)
+                            .cloned()
+                            .collect();
+                        undone_groups = Cow::Owned(new_undone);
+                        Cow::Owned(deletes_from_union.bitxor(deletes_bitxor))
+                    } else {
+                        deletes_from_union
+                    }
+                }
+            }
+        }
+        deletes_from_union
+    }
+
+    fn find_first_undo_candidate_index(
+        &self,
+        toggled_groups: &BTreeSet<usize>,
+    ) -> usize {
+        // find the lowest toggled undo group number
+        if let Some(lowest_group) = toggled_groups.iter().cloned().next() {
+            for (i, rev) in self.revs.iter().enumerate().rev() {
+                if rev.max_undo_so_far < lowest_group {
+                    return i + 1; // +1 since we know the one we just found doesn't have it
+                }
+            }
+            0
+        } else {
+            // no toggled groups, return past end
+            self.revs.len()
+        }
+    }
+
+    fn compute_undo(&self, groups: &BTreeSet<usize>) -> (Revision, Subset) {
+        let toggled_groups = self
+            .undone_groups
+            .symmetric_difference(groups)
+            .cloned()
+            .collect();
+        let first_candidate = self.find_first_undo_candidate_index(&toggled_groups);
+        // the `false` below: don't invert undos since our first_candidate is based on the current undo set, not past
+        let mut deletes_from_union = self
+            .deletes_from_union_before_index(first_candidate, false)
+            .into_owned();
+
+        for rev in &self.revs[first_candidate..] {
+            if let Contents::Edit {
+                ref undo_group,
+                ref inserts,
+                ref deletes,
+                ..
+            } = rev.edit
+            {
+                if groups.contains(undo_group) {
+                    if !inserts.is_empty() {
+                        deletes_from_union =
+                            deletes_from_union.transform_union(inserts);
+                    }
+                } else {
+                    if !inserts.is_empty() {
+                        deletes_from_union =
+                            deletes_from_union.transform_expand(inserts);
+                    }
+                    if !deletes.is_empty() {
+                        deletes_from_union = deletes_from_union.union(deletes);
+                    }
+                }
+            }
+        }
+
+        let deletes_bitxor = self.deletes_from_union.bitxor(&deletes_from_union);
+        let max_undo_so_far = self.revs.last().unwrap().max_undo_so_far;
+        (
+            Revision {
+                max_undo_so_far,
+                edit: Contents::Undo {
+                    toggled_groups,
+                    deletes_bitxor,
+                },
+            },
+            deletes_from_union,
+        )
     }
 }
