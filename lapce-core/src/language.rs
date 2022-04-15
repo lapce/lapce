0cbd259300d7e196f080698fa30ff09d7315e24d
Liberatys <nick.flueckiger@renuo.ch>
2022-04-15 10:32:02 +0200

    Add ruby language support

--- lapce-core/src/language.rs
+++ lapce-core/src/language.rs
@@ -48,6 +48,7 @@ pub enum LapceLanguage {
     C,
     Cpp,
     Json,
+    Ruby,
 }
 
 impl LapceLanguage {
@@ -69,6 +70,7 @@ impl LapceLanguage {
                 LapceLanguage::Cpp
             }
             "json" => LapceLanguage::Json,
+            "rb" => LapceLanguage::Ruby,
             _ => return None,
         })
     }
@@ -88,6 +90,7 @@ impl LapceLanguage {
             LapceLanguage::C => "//",
             LapceLanguage::Cpp => "//",
             LapceLanguage::Json => "",
+            LapceLanguage::Ruby => "#",
         }
     }
 
@@ -106,6 +109,7 @@ impl LapceLanguage {
             LapceLanguage::C => "  ",
             LapceLanguage::Cpp => "    ",
             LapceLanguage::Json => "    ",
+            LapceLanguage::Ruby => "  ",
         }
     }
 
@@ -126,6 +130,7 @@ impl LapceLanguage {
             LapceLanguage::C => tree_sitter_c::language(),
             LapceLanguage::Cpp => tree_sitter_cpp::language(),
             LapceLanguage::Json => tree_sitter_json::language(),
+            LapceLanguage::Ruby => tree_sitter_ruby::language(),
         }
     }
 
@@ -152,6 +157,7 @@ impl LapceLanguage {
             LapceLanguage::C => tree_sitter_c::HIGHLIGHT_QUERY,
             LapceLanguage::Cpp => tree_sitter_cpp::HIGHLIGHT_QUERY,
             LapceLanguage::Json => tree_sitter_json::HIGHLIGHT_QUERY,
+            LapceLanguage::Ruby => tree_sitter_ruby::HIGHLIGHT_QUERY,
         };
 
         HighlightConfiguration::new(language, query, "", "").unwrap()
