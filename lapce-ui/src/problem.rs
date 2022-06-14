6a9d6345188f9f8a4af881e923b6eb1e6883f7c7
Adrian Woźniak <adrian.wozniak@ita-prog.pl>
2022-06-14 14:57:05 +0200

    609: Collapse problem file

--- lapce-ui/src/problem.rs
+++ lapce-ui/src/problem.rs
@@ -72,83 +72,145 @@ impl ProblemContent {
             return;
         }
 
-        let n = (mouse_event.pos.y / self.line_height).floor() as usize;
+        let click_line = (mouse_event.pos.y / self.line_height).floor() as usize;
 
         let items = data.main_split.diagnostics_items(self.severity);
 
-        let mut i = 0;
-        for (path, diagnostics) in items {
-            let diagnostics_len = diagnostics.iter().map(|d| d.lines).sum::<usize>();
-            if diagnostics_len + 1 + i < n {
-                i += diagnostics_len + 1;
-                continue;
+        let mut line_cursor = 0;
+
+        let mut it = items.into_iter().peekable();
+
+        // Skip files before clicked section
+        while let Some((path, diagnostics)) = it.peek() {
+            let is_collapsed =
+                data.problem.collapsed.get(*path).copied().unwrap_or(false);
+            let offset = if is_collapsed {
+                // If section is collapsed count only header with file name
+                1
+            } else {
+                // Total file lines and header with file name
+                diagnostics.iter().map(|d| d.lines).sum::<usize>() + 1 /* file name header */
+            };
+            // did we reached clicked section?
+            if offset + line_cursor <= click_line {
+                // No. Move line cursor and consume file
+                line_cursor += offset;
+                it.next();
+            } else {
+                // Current file is what we are looking for
+                break;
             }
-
-            for d in diagnostics {
-                if i > n {
-                    return;
-                }
-
-                let msg_lines = message_lines(d);
-                let related_lines = related_line_count(d);
-                if i + 1 + msg_lines + related_lines < n {
-                    i += msg_lines + related_lines;
-                    continue;
-                }
-
-                if i < n && n < i + 1 + msg_lines {
-                    ctx.submit_command(Command::new(
-                        LAPCE_UI_COMMAND,
-                        LapceUICommand::JumpToLspLocation(
-                            None,
-                            EditorLocation {
-                                path: path.clone(),
-                                position: Some(d.diagnostic.range.start),
-                                scroll_offset: None,
-                                history: None,
-                            },
-                            false,
-                        ),
-                        Target::Widget(data.id),
-                    ));
-                    return;
-                }
-                i += d.diagnostic.message.split('\n').count();
-
-                for related in d
-                    .diagnostic
-                    .related_information
-                    .as_ref()
-                    .unwrap_or(&Vec::new())
-                {
-                    i += 1;
-                    let lines = related.message.matches('\n').count();
-                    if i <= n && n < i + lines + 1 + 1 {
-                        ctx.submit_command(Command::new(
-                            LAPCE_UI_COMMAND,
-                            LapceUICommand::JumpToLspLocation(
-                                None,
-                                EditorLocation {
-                                    path: related
-                                        .location
-                                        .uri
-                                        .to_file_path()
-                                        .unwrap(),
-                                    position: Some(related.location.range.start),
-                                    scroll_offset: None,
-                                    history: None,
-                                },
-                                false,
-                            ),
-                            Target::Widget(data.id),
-                        ));
-                        return;
-                    }
-                    i += lines;
-                }
-            }
-            i += 1;
         }
+
+        //
+        let (path, diagnostics) = it.next()
+            .expect("Unexpected end of editor diagnostics. We should have found here currently clicked diagnostic message or file name header. This should never happen, please report a bug");
+
+        // handle click on header with file name
+        if line_cursor == click_line {
+            ctx.submit_command(Command::new(
+                LAPCE_UI_COMMAND,
+                LapceUICommand::ToggleProblem(path.to_path_buf()),
+                Target::Widget(data.id),
+            ));
+            return;
+        }
+
+        if data.problem.collapsed.get(path).copied().unwrap_or(false) {
+            log::warn!(
+                "File is collapsed. Can't click any element. This shouldn't happen, please report a bug."
+            );
+            return;
+        }
+
+        // Skip to clicked diagnostic
+        let mut it = diagnostics.into_iter().peekable();
+        while let Some(file_diagnostic) = it.peek() {
+            // Is current diagnostic the clicked one?
+            if line_cursor + file_diagnostic.lines < click_line {
+                // No. Move line cursor and consume diagnostic
+                line_cursor += file_diagnostic.lines;
+                it.next();
+            } else {
+                // We found diagnostic we are looking for
+                break;
+            }
+        }
+
+        // Handle current diagnostic
+        let file_diagnostic = it.next().expect("Editor diagnostic not found. We should find here file diagnostic but nothing left in the array. Please report a bug");
+
+        if line_cursor > click_line {
+            log::error!(
+                "Line cursor is larger than clicked line. This should never happen!"
+            );
+            return;
+        }
+
+        let msg_lines = file_diagnostic.diagnostic.message.lines().count();
+
+        // Widget has mouse about it and line is clicked one.
+        if (line_cursor..(line_cursor + msg_lines)).contains(&click_line) {
+            // rust example: description without location
+            ctx.submit_command(Command::new(
+                LAPCE_UI_COMMAND,
+                LapceUICommand::JumpToLspLocation(
+                    None,
+                    EditorLocation {
+                        path: path.to_path_buf(),
+                        position: Some(file_diagnostic.diagnostic.range.start),
+                        scroll_offset: None,
+                        history: None,
+                    },
+                    false,
+                ),
+                Target::Widget(data.id),
+            ));
+            return;
+        }
+        line_cursor += msg_lines;
+
+        // Skip to clicked related information
+        let mut it = file_diagnostic
+            .diagnostic
+            .related_information
+            .as_deref()
+            .unwrap_or(&[])
+            .iter()
+            .peekable();
+
+        while let Some(related) = it.peek() {
+            let lines = related.message.lines().count() + 1 /*related info will have own file name header with msg location*/;
+            // is current line the clicked one?
+            if (line_cursor..=(line_cursor + lines)).contains(&click_line) {
+                // Yes. Do not move line cursor and stop
+                break;
+            }
+            // No. Move line cursor and consume related info
+            line_cursor += lines;
+            it.next();
+        }
+
+        let related = it.next()
+            .expect("No related information found but something was clicked. This should never happen. Please report a bug.");
+
+        let path = related.location.uri.to_file_path().unwrap();
+        let start = related.location.range.start;
+        let id = data.id;
+        ctx.submit_command(Command::new(
+            LAPCE_UI_COMMAND,
+            LapceUICommand::JumpToLspLocation(
+                None,
+                EditorLocation {
+                    path,
+                    position: Some(start),
+                    scroll_offset: None,
+                    history: None,
+                },
+                false,
+            ),
+            Target::Widget(id),
+        ));
     }
 }
 
@@ -212,14 +274,20 @@ impl Widget<LapceTabData> for ProblemContent {
         _env: &Env,
     ) -> Size {
         let items = data.main_split.diagnostics_items(self.severity);
-        let n = items
+        let lines = items
             .iter()
-            .map(|(_, diagnostics)| {
-                diagnostics.iter().map(|d| d.lines).sum::<usize>() + 1
+            .map(|(path, diagnostics)| {
+                let is_collapsed =
+                    data.problem.collapsed.get(*path).copied().unwrap_or(false);
+                if is_collapsed {
+                    1
+                } else {
+                    diagnostics.iter().map(|d| d.lines).sum::<usize>() + 1 /* file name header */
+                }
             })
             .sum::<usize>();
-        self.line_height = data.config.editor.line_height as f64;
-        self.content_height = self.line_height * n as f64;
+        let line_height = data.config.editor.line_height as f64;
+        self.content_height = line_height * lines as f64;
 
         Size::new(bc.max().width, self.content_height.max(bc.max().height))
     }
@@ -234,11 +302,13 @@ impl Widget<LapceTabData> for ProblemContent {
         let max = (rect.y1 / line_height) as usize + 2;
 
         let items = data.main_split.diagnostics_items(self.severity);
-        let mut i = 0;
+        let mut current_line = 0;
         for (path, diagnostics) in items {
+            let is_collapsed =
+                data.problem.collapsed.get(path).copied().unwrap_or(false);
             let diagnostics_len = diagnostics.iter().map(|d| d.lines).sum::<usize>();
-            if diagnostics_len + 1 + i < min {
-                i += diagnostics_len + 1;
+            if !is_collapsed && diagnostics_len + 1 + current_line < min {
+                current_line += diagnostics_len + 1;
                 continue;
             }
 
@@ -246,7 +316,7 @@ impl Widget<LapceTabData> for ProblemContent {
             let (svg, svg_color) = file_svg(path);
             let rect = Size::new(line_height, line_height)
                 .to_rect()
-                .with_origin(Point::new(0.0, line_height * i as f64))
+                .with_origin(Point::new(0.0, line_height * current_line as f64))
                 .inflate(-padding, -padding);
             ctx.draw_svg(&svg, rect, svg_color);
 
@@ -270,9 +340,15 @@ impl Widget<LapceTabData> for ProblemContent {
                 &text_layout,
                 Point::new(
                     line_height,
-                    line_height * i as f64 + text_layout.y_offset(line_height),
+                    line_height * current_line as f64
+                        + text_layout.y_offset(line_height),
                 ),
             );
+
+            if is_collapsed {
+                current_line += 1;
+                continue;
+            }
 
             let mut path: PathBuf = path.clone();
             if let Some(workspace_path) = data.workspace.path.as_ref() {
@@ -307,29 +383,33 @@ impl Widget<LapceTabData> for ProblemContent {
                     &text_layout,
                     Point::new(
                         x,
-                        line_height * i as f64 + text_layout.y_offset(line_height),
+                        line_height * current_line as f64
+                            + text_layout.y_offset(line_height),
                     ),
                 );
             }
 
             for d in diagnostics {
-                if i > max {
+                if current_line > max {
                     return;
                 }
                 let msg_lines = message_lines(d);
                 let related_lines = related_line_count(d);
-                if i + 1 + msg_lines + related_lines < min {
-                    i += msg_lines + related_lines;
+                if current_line + 1 + msg_lines + related_lines < min {
+                    current_line += msg_lines + related_lines;
                     continue;
                 }
 
-                if ctx.is_hot() && i < mouse_line && mouse_line < i + 1 + msg_lines {
+                if ctx.is_hot()
+                    && current_line < mouse_line
+                    && mouse_line < current_line + 1 + msg_lines
+                {
                     ctx.fill(
                         Size::new(size.width, line_height * msg_lines as f64)
                             .to_rect()
                             .with_origin(Point::new(
                                 0.0,
-                                line_height * (i + 1) as f64,
+                                line_height * (current_line + 1) as f64,
                             )),
                         data.config
                             .get_color_unchecked(LapceTheme::EDITOR_CURRENT_LINE),
@@ -344,7 +424,7 @@ impl Widget<LapceTabData> for ProblemContent {
                     .to_rect()
                     .with_origin(Point::new(
                         line_height,
-                        line_height * (i + 1) as f64,
+                        line_height * (current_line + 1) as f64,
                     ))
                     .inflate(-padding, -padding);
                 ctx.draw_svg(
@@ -356,8 +436,8 @@ impl Widget<LapceTabData> for ProblemContent {
                     ),
                 );
 
-                for line in d.diagnostic.message.split('\n') {
-                    i += 1;
+                for line in d.diagnostic.message.lines() {
+                    current_line += 1;
                     let text_layout = ctx
                         .text()
                         .new_text_layout(line.to_string())
@@ -376,29 +456,26 @@ impl Widget<LapceTabData> for ProblemContent {
                         &text_layout,
                         Point::new(
                             2.0 * line_height,
-                            line_height * i as f64
+                            line_height * current_line as f64
                                 + text_layout.y_offset(line_height),
                         ),
                     );
                 }
 
-                for related in d
-                    .diagnostic
-                    .related_information
-                    .as_ref()
-                    .unwrap_or(&Vec::new())
+                for related in
+                    d.diagnostic.related_information.as_deref().unwrap_or(&[])
                 {
-                    i += 1;
+                    current_line += 1;
 
-                    if ctx.is_hot() && mouse_line >= i {
-                        let lines = related.message.matches('\n').count() + 1 + 1;
-                        if mouse_line < i + lines {
+                    if ctx.is_hot() && mouse_line >= current_line {
+                        let lines = related.message.lines().count() + 1;
+                        if mouse_line < current_line + lines {
                             ctx.fill(
                                 Size::new(size.width, line_height * lines as f64)
                                     .to_rect()
                                     .with_origin(Point::new(
                                         0.0,
-                                        line_height * i as f64,
+                                        line_height * current_line as f64,
                                     )),
                                 data.config.get_color_unchecked(
                                     LapceTheme::EDITOR_CURRENT_LINE,
@@ -412,7 +489,7 @@ impl Widget<LapceTabData> for ProblemContent {
                         .to_rect()
                         .with_origin(Point::new(
                             2.0 * line_height,
-                            line_height * i as f64,
+                            line_height * current_line as f64,
                         ))
                         .inflate(-padding, -padding);
                     ctx.draw_svg(
@@ -423,12 +500,10 @@ impl Widget<LapceTabData> for ProblemContent {
                                 .get_color_unchecked(LapceTheme::EDITOR_FOREGROUND),
                         ),
                     );
+                    let path = path_from_url(&related.location.uri);
                     let text = format!(
                         "{}[{}, {}]:",
-                        path_from_url(&related.location.uri)
-                            .file_name()
-                            .and_then(|f| f.to_str())
-                            .unwrap_or(""),
+                        path.file_name().and_then(|f| f.to_str()).unwrap_or(""),
                         related.location.range.start.line,
                         related.location.range.start.character,
                     );
@@ -450,12 +525,12 @@ impl Widget<LapceTabData> for ProblemContent {
                         &text_layout,
                         Point::new(
                             3.0 * line_height,
-                            line_height * i as f64
+                            line_height * current_line as f64
                                 + text_layout.y_offset(line_height),
                         ),
                     );
-                    for line in related.message.split('\n') {
-                        i += 1;
+                    for line in related.message.lines() {
+                        current_line += 1;
 
                         let text_layout = ctx
                             .text()
@@ -475,14 +550,14 @@ impl Widget<LapceTabData> for ProblemContent {
                             &text_layout,
                             Point::new(
                                 3.0 * line_height,
-                                line_height * i as f64
+                                line_height * current_line as f64
                                     + text_layout.y_offset(line_height),
                             ),
                         );
                     }
                 }
             }
-            i += 1;
+            current_line += 1;
         }
     }
 }
