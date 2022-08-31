aaad96ff055fdb5d42e203061daa4bcac4b12128
Dongdong Zhou <dzhou121@gmail.com>
2022-08-31 19:24:43 +0100

    panel name change

--- lapce-ui/src/explorer.rs
+++ lapce-ui/src/explorer.rs
@@ -347,7 +347,7 @@ impl FileExplorer {
                             .and_then(|p| p.file_name())
                             .and_then(|s| s.to_str())
                             .map(|s| s.to_string())
-                            .unwrap_or_default()
+                            .unwrap_or_else(|| "No Folder Open".to_string())
                             .into(),
                     ),
                     Self::new(data).boxed(),
