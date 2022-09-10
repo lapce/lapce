57baf0abad352153c6dbb5ca0af3d9d4305b3c03
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2022-09-10 17:16:49 +0530

    Consider Alt + Backspace for deletion too (#1120)
    * Update terminal.rs
    
    Fixes https://github.com/lapce/lapce/issues/1119
    
    * Update terminal.rs
    
    * Fix indentation

--- lapce-data/src/terminal.rs
+++ lapce-data/src/terminal.rs
@@ -783,8 +783,10 @@ impl LapceTerminalData {
             Key::Backspace => {
                 Some(if key.mods.ctrl() {
                     "\x08" // backspace
+                } else if key.mods.alt() {
+                    "\x1b\x7f"
                 } else {
-                    "\x7f" // DEL
+                    "\x7f"
                 })
             }
 
