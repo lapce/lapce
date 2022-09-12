de1a928030c5dafd46c2e3d1ac87e8ed98c161be
Suraj Ghimire <suraj@ghishadow.com>
2022-09-12 14:59:52 +0530

    remove injections as we don't have tree-sitter-comment
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- lapce-core/queries/javascript/injections.scm
+++
@@ -1,5 +0,0 @@
-; Parse general tags in single line comments
-
-((comment) @injection.content
- (#set! injection.language "comment")
- (#match? @injection.content "^//"))
--- lapce-core/queries/zig/injections.scm
+++
@@ -1,2 +0,0 @@
-([(line_comment) (doc_comment)] @injection.content
- (#set! injection.language "comment"))
