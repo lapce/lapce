2badaa937f83a14865da6b449b393a7f6867ee8d
Nice Sieve <92229915+NiceSieve@users.noreply.github.com>
2022-09-27 21:58:35 +0300

    run rustfmt

--- lapce-core/src/language.rs
+++ lapce-core/src/language.rs
@@ -354,10 +354,7 @@ const LANGUAGES: &[SyntaxProperties] = &[
         comment: "//",
         indent: "    ",
         code_lens: (DEFAULT_CODE_LENS_LIST, DEFAULT_CODE_LENS_IGNORE_LIST),
-        sticky_headers: &[
-            "function_definition",
-            "struct_specifier",
-        ],
+        sticky_headers: &["function_definition", "struct_specifier"],
         extensions: &["c", "h"],
     },
     #[cfg(feature = "lang-cpp")]
