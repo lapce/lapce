8b108743c0f43ba3d8c909597e79222a23d12e61
Cameron Clark <cameron.clark@hey.com>
2022-10-07 17:32:43 +0100

    WIP

--- lapce-core/src/editor.rs
+++ lapce-core/src/editor.rs
@@ -91,6 +91,31 @@ impl Editor {
                         None
                     };
 
+                    // when text is selected, and [,{,(,'," is inserted
+                    // wrap the text with that char and its corresponding closing pair
+                    if region.start != region.end
+                        && (matching_pair_type.is_some() || c == '"' || c == '\'')
+                    {
+                        if (true == matching_pair_type.unwrap_or(false))
+                            || c == '"'
+                            || c == '\''
+                        {
+                            edits.push((
+                                Selection::region(region.min(), region.min()),
+                                c.to_string(),
+                            ));
+                            edits_after.push((
+                                idx,
+                                match c {
+                                    '"' => '"',
+                                    '\'' => '\'',
+                                    _ => matching_char(c).unwrap(),
+                                },
+                            ));
+                            continue;
+                        }
+                    }
+
                     if auto_closing_matching_pairs {
                         if (c == '"' || c == '\'') && cursor_char == Some(c) {
                             // Skip the closing character
@@ -212,7 +237,7 @@ impl Editor {
                     .map(|(idx, content)| {
                         let region = &selection.regions()[*idx];
                         (
-                            Selection::region(region.start, region.end),
+                            Selection::region(region.max(), region.max()),
                             content.to_string(),
                         )
                     })
