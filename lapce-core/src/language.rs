8fe444b71b71e42cc98ad2d2e7fd8f76a0957ba4
Jakub Panek <me@panekj.dev>
2022-10-08 12:50:00 +0200

    fix: add terraform extension to HCL syntax (#1450)

--- lapce-core/src/language.rs
+++ lapce-core/src/language.rs
@@ -412,7 +412,7 @@ const LANGUAGES: &[SyntaxProperties] = &[
         indent: "  ",
         code_lens: (DEFAULT_CODE_LENS_LIST, DEFAULT_CODE_LENS_IGNORE_LIST),
         sticky_headers: &[],
-        extensions: &["hcl"],
+        extensions: &["hcl", "tf"],
     },
     #[cfg(feature = "lang-html")]
     SyntaxProperties {
@@ -1070,7 +1070,7 @@ mod test {
     }
     #[cfg(feature = "lang-hcl")]
     fn test_hcl_lang() {
-        assert_language(LapceLanguage::Hcl, &["hcl"]);
+        assert_language(LapceLanguage::Hcl, &["hcl", "tf"]);
     }
     #[cfg(feature = "lang-ocaml")]
     fn test_ocaml_lang() {
