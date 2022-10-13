57c16d6ea29a290f679a1cd02676ef882d543fa4
Jordyn Newnham <jordyn@husk.pro>
2022-10-14 05:02:02 +1100

    feature: add shift+C delete remainder of line (#1491)

--- lapce-core/src/command.rs
+++ lapce-core/src/command.rs
@@ -38,6 +38,8 @@ pub enum EditCommand {
     DeleteWordBackward,
     #[strum(serialize = "delete_to_beginning_of_line")]
     DeleteToBeginningOfLine,
+    #[strum(serialize = "delete_to_end_and_insert")]
+    DeleteToEndOfLineAndInsert,
     #[strum(message = "Join Lines")]
     #[strum(serialize = "join_lines")]
     JoinLines,
