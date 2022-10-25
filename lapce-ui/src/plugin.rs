5b256a9efa75f22cbeb9fcb810ae487948738af4
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2022-10-26 04:25:00 +0800

    Plugin tab now accepts keyboard inputs (#1601)
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
    
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- lapce-ui/src/plugin.rs
+++ lapce-ui/src/plugin.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use druid::{
     kurbo::Line,
     piet::{
@@ -13,13 +15,14 @@ use lapce_core::command::FocusCommand;
 use lapce_data::{
     command::{CommandKind, LapceUICommand, LAPCE_COMMAND, LAPCE_UI_COMMAND},
     config::{LapceConfig, LapceIcons, LapceTheme},
-    data::{LapceData, LapceTabData},
+    data::{FocusArea, LapceData, LapceTabData},
     panel::PanelKind,
     plugin::{
         plugin_install_status::PluginInstallType, PluginData, PluginLoadStatus,
         PluginStatus,
     },
     rich_text::RichText,
+    settings::LapceSettingsFocusData,
 };
 
 use crate::{
@@ -625,6 +628,27 @@ impl PluginInfo {
 
         (actual_width - width) / 2.0
     }
+
+    fn request_focus(&self, ctx: &mut EventCtx, data: &mut LapceTabData) {
+        let editor_tab = data
+            .main_split
+            .editor_tabs
+            .get_mut(&self.editor_tab_id)
+            .unwrap();
+        let editor_tab = Arc::make_mut(editor_tab);
+        if let Some(index) = editor_tab
+            .children
+            .iter()
+            .position(|child| child.widget_id() == self.widget_id)
+        {
+            editor_tab.active = index;
+        }
+
+        data.main_split.active_tab = Arc::new(Some(self.editor_tab_id));
+        data.focus = Arc::new(self.widget_id);
+        data.focus_area = FocusArea::Editor;
+        ctx.request_focus();
+    }
 }
 
 impl Widget<LapceTabData> for PluginInfo {
@@ -637,9 +661,28 @@ impl Widget<LapceTabData> for PluginInfo {
         ctx: &mut EventCtx,
         event: &Event,
         data: &mut LapceTabData,
-        _env: &Env,
+        env: &Env,
     ) {
         match event {
+            Event::KeyDown(key_event) => {
+                if ctx.is_focused() {
+                    let mut keypress = data.keypress.clone();
+                    let mut focus = LapceSettingsFocusData {
+                        widget_id: self.widget_id,
+                        editor_tab_id: self.editor_tab_id,
+                        main_split: data.main_split.clone(),
+                        config: data.config.clone(),
+                    };
+                    let mut_keypress = Arc::make_mut(&mut keypress);
+                    let performed_action =
+                        mut_keypress.key_down(ctx, key_event, &mut focus, env);
+                    data.keypress = keypress;
+                    data.main_split = focus.main_split;
+                    if performed_action {
+                        ctx.set_handled();
+                    }
+                }
+            }
             Event::MouseMove(mouse_event) => {
                 if self.status_rect.contains(mouse_event.pos) {
                     ctx.set_cursor(&Cursor::Pointer);
@@ -664,9 +707,16 @@ impl Widget<LapceTabData> for PluginInfo {
             }
             Event::Command(cmd) if cmd.is(LAPCE_UI_COMMAND) => {
                 let cmd = cmd.get_unchecked(LAPCE_UI_COMMAND);
-                if let LapceUICommand::UpdateVoltReadme(text) = cmd {
-                    self.readme_layout.set_text(text.clone());
-                    ctx.request_layout();
+                match cmd {
+                    LapceUICommand::Focus => {
+                        ctx.set_handled();
+                        self.request_focus(ctx, data);
+                    }
+                    LapceUICommand::UpdateVoltReadme(text) => {
+                        self.readme_layout.set_text(text.clone());
+                        ctx.request_layout();
+                    }
+                    _ => {}
                 }
             }
             _ => {}
