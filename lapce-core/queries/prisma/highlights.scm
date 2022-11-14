7cacd0d3511acd0b75c3229025affc4d31e39f75
听风 <1770963469@qq.com>
2022-11-14 12:27:27 +0800

    add prisma`s highlights.scm

---
+++ lapce-core/queries/prisma/highlights.scm
@@ -0,0 +1,56 @@
+(string) @string
+
+(enumeral) @constant
+(number) @constant.numeric
+
+(variable) @variable
+(column_type) @type
+
+(arguments) @variable.other.member
+(model_declaration (identifier) @type)
+
+[
+ "datasource"
+ "enum"
+ "generator"
+ "model"
+ "type"
+] @keyword
+
+[
+ (comment)
+ (developer_comment)
+] @comment
+
+[
+ (attribute)
+ (block_attribute_declaration)
+ (call_expression)
+] @function.builtin
+
+[
+ (true)
+ (false)
+ (null)
+] @constant.builtin.boolean
+
+[
+ "("
+ ")"
+ "["
+ "]"
+ "{"
+ "}"
+] @punctuation.bracket
+
+[
+ ":" 
+ ","
+] @punctuation.delimiter
+
+[
+ "="
+ "@"
+ "@@"
+ (binary_expression)
+] @operator
