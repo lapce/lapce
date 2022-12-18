9e7731aa82bb6cc579be0bae57e2b0684de53470
JustForFun88 <alishergaliev88@gmail.com>
2022-12-18 13:25:19 +0500

    Switch to `once_cell::sync::Lazy`

--- lapce-data/src/completion.rs
+++ lapce-data/src/completion.rs
@@ -6,8 +6,8 @@ use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use itertools::Itertools;
 use lapce_core::command::FocusCommand;
 use lapce_rpc::{buffer::BufferId, plugin::PluginId};
-use lazy_static::lazy_static;
 use lsp_types::{CompletionItem, CompletionResponse, Position};
+use once_cell::sync::Lazy;
 use regex::Regex;
 
 use crate::{config::LapceConfig, list::ListData, proxy::LapceProxy};
@@ -48,12 +48,10 @@ impl Snippet {
     }
 
     fn extract_tabstop(str: &str, pos: usize) -> Option<(SnippetElement, usize)> {
-        lazy_static! {
-            // Regex for `$...` pattern, where `...` is some number (for example `$1`)
-            static ref REGEX_FIRST: Regex = Regex::new(r#"^\$(\d+)"#).unwrap();
-            // Regex for `${...}` pattern, where `...` is some number (for example `${1}`)
-            static ref REGEX_SECOND: Regex = Regex::new(r#"^\$\{(\d+)\}"#).unwrap();
-        }
+    // Regex for `$...` pattern, where `...` is some number (for example `$1`)
+    static REGEX_FIRST: Lazy<Regex> = Lazy::new(|| Regex::new(r#"^\$(\d+)"#).unwrap());
+    // Regex for `${...}` pattern, where `...` is some number (for example `${1}`)
+    static REGEX_SECOND: Lazy<Regex> = Lazy::new(|| Regex::new(r#"^\$\{(\d+)\}"#).unwrap());
 
         let str = &str[pos..];
         if let Some(matched) = REGEX_FIRST.find(str) {
