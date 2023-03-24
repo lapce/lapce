bff327f65b5421b7bf27e82baa1a35868fd9c209
tk103331 <tk103331@gmail.com>
2023-03-24 14:56:07 +0800

    fix: search result folding incorrectly

--- lapce-ui/src/search.rs
+++ lapce-ui/src/search.rs
@@ -340,11 +340,15 @@ impl SearchContent {
             };
             if i == n {
                 self.file_folds.insert(path.clone(), !fold);
-                ctx.request_paint();
+                ctx.request_layout();
                 break;
             }
             i += 1;
             if !fold {
+                if i + matches.len() < n {
+                    i += matches.len();
+                    continue;
+                }
                 for (line_number, (start, _end), _line) in matches {
                     if i == n {
                         ctx.submit_command(Command::new(
@@ -474,17 +478,18 @@ impl Widget<LapceTabData> for SearchContent {
         let focus_color = data.config.get_color_unchecked(LapceTheme::EDITOR_FOCUS);
         let mut i = 0;
         for (path, matches) in data.search.matches.iter() {
-            if matches.len() + 1 + i < min {
-                i += matches.len() + 1;
-                continue;
-            }
-
             let svg_size = data.config.ui.icon_size() as f64;
             let fold = if let Some(fold) = self.file_folds.get(path) {
                 *fold
             } else {
                 false
             };
+            let children_len = if fold { 0 } else { matches.len() };
+            if i + children_len + 1 < min {
+                i += children_len + 1;
+                continue;
+            }
+
             let fold_icon_name = if !fold {
                 LapceIcons::ITEM_OPENED
             } else {
