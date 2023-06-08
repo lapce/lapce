a5729ceff43fd50cd213a9c4cbd1137b67de2e3b
tk103331 <tk103331@gmail.com>
2023-06-08 16:54:48 +0800

    code fmt

--- lapce-app/src/window_tab.rs
+++ lapce-app/src/window_tab.rs
@@ -485,9 +485,8 @@ impl WindowTabData {
                         path: None,
                         last_open: 0,
                     };
-                    window_command.set(Some(
-                        WindowCommand::SetWorkspace { workspace },
-                    ));
+                    window_command
+                        .set(Some(WindowCommand::SetWorkspace { workspace }));
                 }
             }
             OpenFile => {}
