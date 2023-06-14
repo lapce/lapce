b27b3dbde6739cc535a3712d21c18711912dd8a8
Dongdong Zhou <dzhou121@gmail.com>
2023-06-14 21:09:26 +0100

    add DiffEditorId

--- lapce-app/src/editor/diff.rs
+++ lapce-app/src/editor/diff.rs
@@ -3,11 +3,16 @@ use std::path::PathBuf;
 use floem::reactive::{create_effect, create_rw_signal, Scope};
 use lapce_core::buffer::DiffLines;
 
-use crate::{doc::Document, id::EditorId, window_tab::CommonData};
+use crate::{
+    doc::Document,
+    id::{DiffEditorId, EditorId},
+    window_tab::CommonData,
+};
 
 use super::EditorData;
 
 pub struct DiffEditorData {
+    pub id: DiffEditorId,
     pub left: EditorData,
     pub right: EditorData,
 }
@@ -15,6 +20,7 @@ pub struct DiffEditorData {
 impl DiffEditorData {
     pub fn new(
         cx: Scope,
+        id: DiffEditorId,
         left_doc: Document,
         right_doc: Document,
         common: CommonData,
@@ -27,7 +33,7 @@ impl DiffEditorData {
 
         create_effect(cx, move |_| {});
 
-        Self { left, right }
+        Self { id, left, right }
     }
 }
 
