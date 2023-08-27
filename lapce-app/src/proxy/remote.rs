5729496df17ede2a380cdf3ad07e9f2e23b49237
Jakub Panek <me@panekj.dev>
2023-08-27 11:11:08 +0200

    Revert "Merge pull request from GHSA-v7q3-c3cf-w238"
    This reverts commit 520a6deb55f6df2a68d58e25934d36e4735ef1ea.

--- lapce-app/src/proxy/remote.rs
+++ lapce-app/src/proxy/remote.rs
@@ -261,31 +261,37 @@ fn download_remote(
             cmd.status
         }
         _ => {
-            let proxy_script = UNIX_PROXY_SCRIPT.to_owned();
-            let proxy_script =
-                base64::engine::general_purpose::STANDARD.encode(UNIX_PROXY_SCRIPT);
+            let local_proxy_script =
+                Directory::proxy_directory().unwrap().join("proxy.sh");
 
-            let version = if meta::VERSION == "debug" {
-                "nightly"
-            } else {
-                meta::VERSION
-            };
-            let mut cmd = remote
+            let mut proxy_script = std::fs::OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(&local_proxy_script)?;
+            proxy_script.write_all(UNIX_PROXY_SCRIPT)?;
+
+            let remote_proxy_script = "/tmp/lapce-proxy.sh";
+            remote.upload_file(local_proxy_script, remote_proxy_script)?;
+
+            let cmd = remote
+                .command_builder()
+                .args(["chmod", "+x", remote_proxy_script])
+                .output()?;
+            debug!("{}", String::from_utf8_lossy(&cmd.stderr));
+            debug!("{}", String::from_utf8_lossy(&cmd.stdout));
+
+            let cmd = remote
                 .command_builder()
                 .args([
-                    "echo",
-                    &proxy_script,
-                    "|",
-                    "base64",
-                    "-d",
-                    "|",
-                    "sh",
-                    "/dev/stdin",
-                    version,
+                    remote_proxy_script,
+                    if meta::VERSION == "debug" {
+                        "nightly"
+                    } else {
+                        meta::VERSION
+                    },
                     remote_proxy_path,
                 ])
                 .output()?;
-
             debug!("{}", String::from_utf8_lossy(&cmd.stderr));
             debug!("{}", String::from_utf8_lossy(&cmd.stdout));
 
