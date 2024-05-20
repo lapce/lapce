902652b952ec47f1150b3417f07d04e25f554b5c
Jakub Panek <me@panekj.dev>
2024-05-20 05:06:13 +0200

    implement log level filtering from settings

--- lapce-app/src/command.rs
+++ lapce-app/src/command.rs
@@ -682,9 +682,6 @@ pub enum InternalCommand {
     SetModal {
         modal: bool,
     },
-    UpdateLogLevel {
-        level: tracing_subscriber::filter::LevelFilter,
-    },
     OpenWebUri {
         uri: String,
     },
